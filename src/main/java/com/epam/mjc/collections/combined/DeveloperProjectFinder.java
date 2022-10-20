143ba9d8076ab61f82fcf067987ebe290f3c75b9
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-20 07:05:52 +0500

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,33 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+
+
+        List<String> res =
+                projects.entrySet().stream()
+                        .filter(prj -> prj.getValue().contains(developer))
+                        .map(Map.Entry::getKey)
+                        .collect(Collectors.toList());
+
+
+        Comparator<String> comparator = (o1, o2) -> {
+            int res1 = o2.length() - o1.length();
+
+            if (res1 == 0) {
+                res1 = o2.compareTo(o1);
+            }
+
+            return res1;
+        };
+
+        res.sort(comparator);
+
+
+        return res;
+
     }
 }
