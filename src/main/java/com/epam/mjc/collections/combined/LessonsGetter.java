84264fa34386333f00d1b61b34bf7cb40ba8410d
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-20 07:06:16 +0500

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+
+
+        Set<String>res=new HashSet<>();
+
+        for (Map.Entry<String, List<String>> stringListEntry : timetable.entrySet()) {
+
+            if(stringListEntry.getValue().isEmpty())continue;
+            List<String> value = stringListEntry.getValue();
+            res.addAll(value);
+            res.add(value.get(0));
+        }
+
+        return res;
     }
 }
